2b92e9cfa452ccbd135d9d09f4abf5a79f2d9962
David Juaneda <dajuayen@gmail.com>
2022-02-01 17:44:00 +0100

    [ADD] Postgres
    Need to specify environment variable:
    env -> postgres
    By default it will be
    sent -> test
    which use Sqlite as database.

--- config/database.py
+++ config/database.py
@@ -2,17 +2,39 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+from config.settings import Settings
+
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://admin:admin@postgresserver/db"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args={
-        "check_same_thread": False,
-        # "pool_size": 20,
-        # "max_overflow": 2
-    }
-)
+settings = Settings()
+
+
+def _get_url(env):
+    if env == 'test':
+        return "sqlite:///./sql_app.db"
+    elif env == 'postgres':
+        return "postgresql://admin:admin@localhost:5432/customers_db"
+
+
+def _get_engine_attrs(env):
+    if env == 'test':
+        return {"connect_args": {
+            "check_same_thread": False,
+        }}
+    elif env == 'postgres':
+        return {'pool_pre_ping': True}
+
+
+def get_engine(settings):
+    return create_engine(
+        _get_url(settings.env),
+        **_get_engine_attrs(settings.env)
+    )
+
+
+engine = get_engine(settings)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
